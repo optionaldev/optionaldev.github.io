a81938fcfa1a59772c57df58e28bb962ca127491
佐藤悠真 <archyusa@icloud.com>
2021-05-24 22:33:10 +0200

    Added light mode

---
+++ common.js
@@ -0,0 +1,4 @@
+/*!
+ * Minify & Encode by http://www.MinifyWeb.com/
+ */
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('i 0=1;9 a(6){7(0==1){5.4.3.8("2");6.b()}c{0=1}}9 e(){0=d;7(f.g==\'h\'){5.4.3.8("2")}}',19,19,'keyupCalled|false|lightmode|classList|documentElement|document|input|if|toggle|function|clicky|blur|else|true|keyup|event|code|Space|var'.split('|'),0,{}))
